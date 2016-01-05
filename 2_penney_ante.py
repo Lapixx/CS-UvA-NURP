60a69cf9d8516c033273d85edc7e329298ee6f30
Tijn Kersjes <tijn@divbyzero.nl>
2016-01-05 16:42:53 +0100

    Stats

--- 2_penney_ante.py
+++ 2_penney_ante.py
@@ -21,13 +21,9 @@ def runMatch(players):
     seq = "." * n
 
     rounds = 0
-    print "Flipping coins:"
     while seq != ps[0] and seq != ps[1]:
         rounds += 1
         seq = flipCoin(seq)
-        sys.stdout.write(seq[n - 1])
-        sys.stdout.flush()
-    sys.stdout.write("\n\n")
 
     winner = 0 if seq == ps[0] else 1
 
@@ -36,10 +32,39 @@ def runMatch(players):
 def main():
 
     players = ["HTT", "HHT"]
-    rounds, winner = runMatch(players)
+    iterations = 100000
 
-    winner = "Anna" if winner == 1 else "Bob"
-    print winner, "wins after", rounds, "rounds."
+    total_rounds = 0
+    total_anna_wins = 0
+    rounds_if_anna_wins = 0
+    total_bob_wins = 0
+    rounds_if_bob_wins = 0
+
+    for i in range(iterations):
+
+        rounds, winner = runMatch(players)
+
+        total_rounds += rounds
+        if winner == 0:
+            total_bob_wins += 1
+            rounds_if_bob_wins += rounds
+        else:
+            total_anna_wins += 1
+            rounds_if_anna_wins += rounds
+
+        sys.stdout.write("\rFlipping coins... " + str(100 * (i+1) / iterations) + "% ")
+        sys.stdout.flush()
+
+    avg_rounds = float(total_rounds) / iterations
+    frac_anna_wins = 100 * total_anna_wins / iterations
+    frac_bob_wins = 100 * total_bob_wins / iterations
+    avg_rounds_if_anna_wins = float(rounds_if_anna_wins) / total_anna_wins
+    avg_rounds_if_bob_wins = float(rounds_if_bob_wins) / total_bob_wins
+
+    print "\n"
+    print "Average number of rounds:", avg_rounds
+    print "Wins by Anna:", str(frac_anna_wins) + "%", "(taking", avg_rounds_if_anna_wins, "rounds on average)"
+    print "Wins by Bob:", str(frac_bob_wins) + "%", "(taking", avg_rounds_if_bob_wins, "rounds on average)"
 
 if __name__ == "__main__":
     main()
