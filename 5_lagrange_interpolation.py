0b69775ca265ea1d89016e82693e0746863528cd
Tijn Kersjes <tijn@divbyzero.nl>
2016-01-08 18:36:39 +0100

    Second lagrange assignment

--- 5_lagrange_interpolation.py
+++ 5_lagrange_interpolation.py
@@ -10,6 +10,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.interpolate as ip
 
+# -------------------- Utils --------------------
+
 def unzip(points):
     points_x = []
     points_y = []
@@ -31,6 +33,8 @@ def lagrange(points, x):
                 a *= (x - xs[j]) / (xs[i] - xs[j])
         y += a
     return y
+
+# -------------------- Assignment A --------------------
 
 def showGraph(points, steps, zoom):
     degree = len(points) - 1
@@ -60,12 +64,49 @@ def showGraph(points, steps, zoom):
 
     plt.show()
 
-def main():
+def assignmentA():
     points = [(0, 2), (2, 1), (1, 2.3), (3, -1), (5.8, 3.5), (6.5, 0.5), (4.5, 2)]
     steps = 100
     zoom = 0.5
     for i in range(len(points)):
         showGraph(points[:i + 1], steps, zoom)
+
+# -------------------- Assignment B --------------------
+
+def f(t):
+    return 1.0 / (t ** 2 + 1.0)
+
+def getPoints(n):
+    points = []
+    for i in range(n + 1):
+        x = -5.0 + 10.0 * i / n
+        y = f(x)
+        points += [(x, y)]
+    return points
+
+def assignmentB():
+    steps = 100
+    rangex = np.linspace(-6, 6, steps)
+
+    points_a = getPoints(14)
+    points_b = getPoints(6)
+
+    axs, ays = unzip(points_a)
+    bxs, bys = unzip(points_b)
+
+    plt.plot(rangex, map(lambda x: lagrange(points_a, x), rangex), 'r-', rangex, map(lambda x: lagrange(points_b, x), rangex), 'b-', axs, ays, 'ro', bxs, bys, 'bo')
+    plt.axis([-6, 6, -1, 5])
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.title('Approximate f(t) = 1 / (t^2 + 1)')
+
+    plt.show()
+
+# -------------------- Entry Point --------------------
+
+def main():
+    assignmentA()
+    assignmentB()
 
 if __name__ == "__main__":
     main()
